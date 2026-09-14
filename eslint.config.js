c72c868d5d1892a705e26b8f2c3742740cd720b7
Gmcjr <gmcostajr@gmail.com>
2026-09-14 01:37:58 -0500

    Add / ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', '.astro/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['src/**/*.{ts,tsx}', 'astro.config.ts'],
+    extends: [...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  ...astro.configs['flat/recommended'],
+  eslintConfigPrettier
+);
